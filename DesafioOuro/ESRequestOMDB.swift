07bdd21e85551b99cff84542db6b92ff65a07399
Student <lucas_dilts@hotmail.com>
2016-03-14 09:14:32 -0300

    [ldilts] Forced merge

--- DesafioOuro/ESRequestOMDB.swift
+++ DesafioOuro/ESRequestOMDB.swift
@@ -9,70 +9,60 @@
 import Foundation
 import Alamofire
 import RealmSwift
+import SwiftyJSON
 
 
 class ESRequestOMDB {
     
-    var filmeDictionary:NSDictionary!
+    var movie:Movie!
     var urlRequest:String!
+    var json:JSON!
     
-    func request(url: String) ->NSDictionary{
-        urlRequest = url
+    let realm = try! Realm()
+    
+    init() {
         
-        makeCall() {
-            (result: (NSDictionary?, NSError?)) in
-            
-            let api = ESRequestOMDB()
-            api.getOrders() { responseObject, error in
-                // use responseObject and error here
-                self.filmeDictionary = responseObject
-                return
+    }
+    
+    func request(url: String?) {
+        
+        if let _ = url {
+            urlRequest = url
+        
+            self.makeCall { (movie) ->  Void in
+                if let unwrappedMovie = movie {
+                    try! self.realm.write {
+                        self.realm.add(unwrappedMovie)
+                    }
+                    
+                    self.movie = unwrappedMovie
+                    NSNotificationCenter.defaultCenter().postNotificationName("MovieFetched", object: nil)
+                }
             }
         }
-     
-        let filme:Filme = Filme()
-        
-        filme.Title = (filmeDictionary.valueForKey("Title") as! String)
-        filme.Year  = (filmeDictionary.valueForKey("Year") as! String)
-        filme.Rated = (filmeDictionary.valueForKey("Rated") as! String)
-        filme.Released = (filmeDictionary.valueForKey("Released") as! String)
-        filme.Runtime = (filmeDictionary.valueForKey("Runtime") as! String)
-        filme.Genre = (filmeDictionary.valueForKey("Genre") as! String)
-        filme.Writer = (filmeDictionary.valueForKey("Writer") as! String)
-        filme.Actors = (filmeDictionary.valueForKey("Actors") as! String)
-        filme.Plot = (filmeDictionary.valueForKey("Plot") as! String)
-        filme.Language = (filmeDictionary.valueForKey("Language") as! String)
-        filme.Poster = (filmeDictionary.valueForKey("Poster") as! String)
-        filme.imdbID = (filmeDictionary.valueForKey("imdbID") as! String)
-        filme.Type = (filmeDictionary.valueForKey("movie") as! String)
-        
-        let realm = try! Realm()
-
-        try! realm.write {
-            realm.add(filme)
-        }
-
-        
-        return filmeDictionary
-    
-    }
-
-    
-    func getOrders(completionHandler: (NSDictionary?, NSError?) -> ()) {
-        makeCall(completionHandler)
     }
     
-    func makeCall(completionHandler: (NSDictionary?, NSError?) -> ()) {
+    
+    func makeCall(completionHandler: (Movie?) -> ()) {
         let params = ["consumer_key":"key", "consumer_secret":"secret"]
-        
         Alamofire.request(.GET, urlRequest, parameters: params)
-            .responseJSON { response in
-                switch response.result {
-                case .Success(let value):
-                    completionHandler(value as? NSDictionary, nil)
-                case .Failure(let error):
-                    completionHandler(nil, error)
+            .validate()
+            .responseJSON { (response) in
+                
+                guard response.result.isSuccess else{
+                    completionHandler(nil)
+                    return
                 }
+                
+                guard let _ = response.result.value as? [String : AnyObject] else{
+                    completionHandler(nil)
+                    return
+                }
+                
+                let json = JSON(response.result.value!)
+                print(json)
+
+                completionHandler(Movie(withJson: json))
         }
     }
     
