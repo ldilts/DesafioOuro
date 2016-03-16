4ae3c8aa487c49d7e18c748f7e7d91b12e4daa4a
Fernando Matheus <fernandomatheus@mac-mini-de-fundacao-7.local>
2016-03-16 14:27:50 -0300

    [Fernando] Enviando o Chat - Novo storyboard - Chat.storyboard e a classe de comportamentos - ChatController.swift

---
+++ DesafioOuro/ChatController.swift
@@ -0,0 +1,99 @@
+//
+//  ChatController.swift
+//  DesafioOuro
+//
+//  Created by Fernando Matheus on 16/03/16.
+//  Copyright © 2016 Student. All rights reserved.
+//
+
+import UIKit
+
+class ChatController: UIViewController, UITableViewDelegate, UITableViewDataSource, UITextFieldDelegate {
+
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var messageBar: UIView!
+    @IBOutlet weak var topViewHeigthConstraint: NSLayoutConstraint!
+    @IBOutlet weak var spacerView: UIView!
+    @IBOutlet weak var topView: UIView!
+    
+    
+    @IBAction func actionSend(sender: AnyObject) {
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()        
+        
+        
+        self.textField.delegate = self
+
+        // Do any additional setup after loading the view.
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "keyboardDidHide:", name: UIKeyboardDidHideNotification, object: nil)
+        
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "keyboardDidChange:", name: UIKeyboardDidChangeFrameNotification, object: nil)
+    }
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func keyboardDidHide(notification: NSNotification) {
+        
+    }
+    
+    func keyboardDidChange(notification: NSNotification) {
+        
+        if let userInfo = notification.userInfo {
+            if let _ = userInfo[UIKeyboardFrameEndUserInfoKey]?.CGRectValue {
+                
+                var keyboardFrameEnd = userInfo[UIKeyboardFrameEndUserInfoKey]?.CGRectValue
+                keyboardFrameEnd = self.view.convertRect(keyboardFrameEnd!, fromView: nil)
+                
+                UIView.animateWithDuration(0.1, delay: 0.0, options: UIViewAnimationOptions.BeginFromCurrentState, animations: { () -> Void in
+                    
+                    self.topViewHeigthConstraint.constant = (self.view.frame.size.height - keyboardFrameEnd!.origin.y)
+                    self.view.setNeedsLayout()
+                    self.view.layoutIfNeeded()
+                    }, completion: { (success: Bool) -> Void in
+                })
+            }
+        }
+        
+    }
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
